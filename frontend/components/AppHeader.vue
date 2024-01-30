adb8ff4bb283ef75baaac8a4efeeb16eac4847da
paulbriton <paull.briton@gmail.com>
2024-01-31 00:02:08 +0100

    Refactor header into components

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,38 +1,11 @@
-<script setup>
-const colorMode = useColorMode()
-const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ? 'dark' : 'light'))
-</script>
-
 <template>
   <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700 shadow">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> -->
-        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 48 48"
-          xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <component :is="'style'">.b{fill:#AB7C94;stroke:#000000;stroke-linecap:round;stroke-linejoin:round;}
-            </component>
-          </defs>
-          <g>
-            <path class="b" d="M24,24.4682H5.5c0,9.2155,11.597,17.1393,11.597,17.1393h6.903" />
-            <path class="b" d="M10.8843,26.5736c0,6.7924,7.6278,11.8041,7.6278,11.8041" />
-            <path class="b" d="M24,24.4682h18.5c0,9.2155-11.597,17.1393-11.597,17.1393h-6.903" />
-            <path class="b"
-              d="M14.0942,24.4682c-1.3461-5.0392,7.6541-8.5034,15.7305-9.5733l.7892-1.4542,3.184,3.87-1.7473,.2847c-.8542,2.8087-5.1347,6.8728-5.1347,6.8728" />
-            <path class="b" d="M15.5093,24.4682c.2883-2.5946,3.2968-5.5138,14.7724-9.0189" />
-            <path class="b" d="M30.5784,15.8092c-4.3403,2.1744-9.4,4.6725-12.9464,8.659" />
-            <path class="b" d="M31.4253,16.8367c-4.5094,6.1259-7.4253,7.6315-7.4253,7.6315" />
-            <path class="b" d="M21.5047,24.4682c2.7196-1.5897,9.4897-8.1542,9.4897-8.1542" />
-            <path class="b"
-              d="M33.0768,16.4532l8.1513-6.6057c.5628-.4561,.6493-1.282,.1932-1.8447h0c-.4561-.5628-1.282-.6493-1.8447-.1932l-8.1513,6.6057" />
-          </g>
-          <line class="b" x1="29.8248" y1="14.8949" x2="32.0506" y2="17.5954" />
-        </svg>
-
+    <div class="flex flex-wrap items-center grow mx-auto p-4">
+      <a href="" class="flex items-center justify-center grow space-x-3 rtl:space-x-reverse">
+        <AppLogo />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Apicius</span>
       </a>
-      <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <div class="flex items-center justify-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse mr-4">
         <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
           <span class="sr-only">Open user menu</span>
           <img class="w-8 h-8 rounded-full" src="/profile-picture-3.jpg" alt="user photo">
@@ -65,7 +38,7 @@ const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ?
           </svg>
         </button>
       </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+      <div class="items-center grow hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
         <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 dark:border-gray-700">
           <li>
             <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
@@ -78,18 +51,29 @@ const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ?
           </li>
           <li>
             <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Ingredients</a>
-      </li>
-      </ul>
+          </li>
+        </ul>
+        <div class="space-x-8 w-1/3">
+          <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          </svg>
+          <span class="sr-only">Search</span>
+          </button>
+          <div class="relative hidden md:block">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+              <span class="sr-only">Search icon</span>
+            </div>
+            <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+          </div>
+        </div>
+        
       </div>
-      <div class="flex items-center md:order-3">
-        <button aria-label="Color Mode" class="inline-block text-sm px-2 py-2 leading-none border rounded text-white border-white dark:border-black hover:border-transparent bg-teal-800 mt-4 lg:mt-0"
-                @click="changeColor">
-            <ColorScheme placeholder="...">
-                <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" class="text-sm text-black" />
-                <Icon v-else name="heroicons-outline:sun" class="text-md" />
-            </ColorScheme>
-        </button>
-    </div>
+
+      <ColorSchemeBtn />
     </div>
   </nav>
 </template>
---
+++ frontend/components/ColorSchemeBtn.vue
@@ -0,0 +1,16 @@
+<script setup>
+const colorMode = useColorMode()
+const changeColor = () => (colorMode.preference = (colorMode.value === 'light' ? 'dark' : 'light'))
+</script>
+
+<template>
+    <div class="flex-none md:order-3 grow-0">
+        <button aria-label="Color Mode" class="inline-block text-sm px-2 py-2 leading-none border rounded text-gray-900 bg-gray-100 border-gray:200 dark:border-gray-900 hover:border-transparent dark:bg-teal-800 mt-4 lg:mt-0"
+                @click="changeColor">
+            <ColorScheme placeholder="...">
+                <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" class="text-md " />
+                <Icon v-else name="heroicons-outline:sun" class="text-md" />
+            </ColorScheme>
+        </button>
+    </div>
+</template>
